99d14f596ac76f4ad23de0c172dac2d274856251
daemonseal <daemonseal@pm.me>
2020-03-09 02:56:15 +0700

    Add wifi option

--- swifty-cayenne.ino
+++ swifty-cayenne.ino
@@ -9,8 +9,10 @@
 #define FOG_PIN 5
 
 // WiFi network info.
-char ssid[] = "sabaiheart";
-char wifiPassword[] = "ok0boVu1";
+//char ssid[] = "sabaiheart";
+//char wifiPassword[] = "ok0boVu1";
+char ssid[] = "IoT-dragino";
+char wifiPassword[] = "IoT12345";
 
 // Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
 char username[] = "9a837da0-47db-11ea-b73d-1be39589c6b2";
@@ -31,31 +33,25 @@ void loop() {
   Cayenne.loop();
 }
 
-// Default function for sending sensor data at intervals to Cayenne.
-// You can also use functions for specific channels, e.g CAYENNE_OUT(1) for sending channel 1 data.
 CAYENNE_OUT_DEFAULT()
 {
   // Write data to Cayenne here. This example just sends the current uptime in milliseconds on virtual channel 0.
   Cayenne.virtualWrite(0, millis());
-  // Some examples of other functions you can use to send data.
   //Cayenne.celsiusWrite(1, 22.0);
   //Cayenne.luxWrite(2, 700);
   //Cayenne.virtualWrite(3, 50, TYPE_PROXIMITY, UNIT_CENTIMETER);
 }
 
-// Default function for processing actuator commands from the Cayenne Dashboard.
-// You can also use functions for specific channels, e.g CAYENNE_IN(1) for channel 1 commands.
-//CAYENNE_IN_DEFAULT()
-//{
-//  CAYENNE_LOG("Channel %u, value %s", request.channel, getValue.asString());
-//  //Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
-//}
 
-// This function is called when data is sent from Cayenne.
+CAYENNE_IN_DEFAULT()
+{
+  CAYENNE_LOG("Channel %u, value %s", request.channel, getValue.asString());
+  getValue.setError("Error occured!");
+}
+
 CAYENNE_IN(AIR_CHANNEL)
 {
   Serial.println("Incoming from Cayenne");
-  // Write value to turn the relay switch on or off. This code assumes you wire your relay as normally open.
   int value = getValue.asInt();
   digitalWrite(AIR_PIN, value);
 }
@@ -63,7 +59,6 @@ CAYENNE_IN(AIR_CHANNEL)
 CAYENNE_IN(FOG_CHANNEL)
 {
   Serial.println("Incoming from Cayenne");
-  // Write value to turn the relay switch on or off. This code assumes you wire your relay as normally open.
   int value = getValue.asInt();
   digitalWrite(FOG_PIN, value);
 }
