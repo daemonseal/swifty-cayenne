1099ed6e2b904c5ec6e075a6a0b87d30665f6be7
smoogi <spacecorgi@pm.me>
2020-03-31 13:04:03 +0700

    Update

--- swifty-cayenne.ino
+++ swifty-cayenne.ino
@@ -2,19 +2,20 @@
 #define CAYENNE_PRINT Serial
 #include <CayenneMQTTESP8266.h>
 
+// Connected to RELAY INPUT 1 (Pink outlet)
 #define AIR_CHANNEL 4
 #define AIR_PIN 4
+#define AUTO_AIR_CHANNEL 8
 
+// Connected to RELAY INPUT 2 (Green outlet)
 #define FOG_CHANNEL 5
 #define FOG_PIN 5
+#define AUTO_FOG_CHANNEL 10
 
 // WiFi network info.
-//char ssid[] = "sabaiheart";
-//char wifiPassword[] = "ok0boVu1";
 char ssid[] = "IoT-dragino";
 char wifiPassword[] = "IoT12345";
 
-// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
 char username[] = "9a837da0-47db-11ea-b73d-1be39589c6b2";
 char password[] = "27aa25d313921d02945b9dacda3ddb6d2a516053";
 char clientID[] = "4105f260-5543-11ea-a38a-d57172a4b4d4";
@@ -35,11 +36,9 @@ void loop() {
 
 CAYENNE_OUT_DEFAULT()
 {
-  // Write data to Cayenne here. This example just sends the current uptime in milliseconds on virtual channel 0.
-  Cayenne.virtualWrite(0, millis());
-  //Cayenne.celsiusWrite(1, 22.0);
-  //Cayenne.luxWrite(2, 700);
-  //Cayenne.virtualWrite(3, 50, TYPE_PROXIMITY, UNIT_CENTIMETER);
+  // Write uptime in seconds to Cayenne
+  unsigned long seconds = millis();
+  Cayenne.virtualWrite(0, seconds);
 }
 
 
@@ -52,6 +51,7 @@ CAYENNE_IN_DEFAULT()
 CAYENNE_IN(AIR_CHANNEL)
 {
   Serial.println("Incoming from Cayenne");
+  Serial.println("MANUAL EXHAUST FAN");
   int value = getValue.asInt();
   digitalWrite(AIR_PIN, value);
 }
@@ -59,6 +59,33 @@ CAYENNE_IN(AIR_CHANNEL)
 CAYENNE_IN(FOG_CHANNEL)
 {
   Serial.println("Incoming from Cayenne");
+   Serial.println("MANUAL HUMIDIFIER");
   int value = getValue.asInt();
   digitalWrite(FOG_PIN, value);
+}
+
+CAYENNE_IN(AUTO_AIR_CHANNEL)
+{
+  Serial.println("Incoming from Cayenne");
+  Serial.println("AUTO EXHAUST FAN");
+  int value = getValue.asInt();
+  if(value == 1){
+    digitalWrite(FOG_PIN, LOW);
+    digitalWrite(AIR_PIN, HIGH);
+    delay(12500);
+  }
+  digitalWrite(AIR_PIN, LOW);
+}
+
+CAYENNE_IN(AUTO_FOG_CHANNEL)
+{
+  Serial.println("Incoming from Cayenne");
+  Serial.println("AUTO HUMIDIFIER");
+  int value = getValue.asInt();
+  if(value == 1){
+    digitalWrite(AIR_PIN, LOW);
+    digitalWrite(FOG_PIN, HIGH);
+    delay(12500);
+  }
+  digitalWrite(FOG_PIN, LOW);
 }
